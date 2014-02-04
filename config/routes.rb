314a20113fb7ed3d22f23fa6d0e1dd4e04ffa5a0
Ag <github@assembledgoods.com>
2014-02-03 21:52:28 -0600

    rails 4 updates, removing attr_accessible, setting via when using match routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Spree::Core::Engine.routes.prepend do
     end
   end
   
-  match "/reviews/all_list", :to => "reviews#all_list", :as => :all_reviews
+  match "/reviews/all_list", :to => "reviews#all_list", :as => :all_reviews, :via => [:get, :post]
   
 end
